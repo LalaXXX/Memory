70eeb83d80cb5849f6a223e1569f6ef265e415df
LalaX <lalaxiang66@gmail.com>
2020-02-26 14:56:37 +0800

    add comment

--- jump7.py
+++ jump7.py
@@ -1,5 +1,5 @@
 for i in range(1,101):
-    if i % 7 == 0 or  i //10 == 7 or i % 10  == 7:
+    if i % 7 == 0 or  i //10 == 7 or i % 10  == 7:#Add comment
         continue
     else:
         print(i)
